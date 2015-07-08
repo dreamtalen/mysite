05478d0d6ae0b790f903046aa9b5e95d63ac36b9
dreamtalen <dreamtalen@hotmail.com>
2015-07-08 14:05:27 +0800

    i dunno, maybe this works

--- polls/views.py
+++ polls/views.py
@@ -1,20 +1,37 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404, render
 
 # Create your views here.
-from django.http import HttpResponse
-from .models import Question
+from .models import Choice, Question
+from django.http import HttpResponse, Http404, HttpResponseRedirect
+from django.template import RequestContext, loader
+from django.core.urlresolvers import reverse
+from django.views import generic
 
-def index(request):
-	latest_question_list = Question.objects.order_by('-pub_date')[:5]
-	output = ','.join([p.question_text for p in latest_question_list])
-	return HttpResponse(output)
+class IndexView(generic.ListView):
+	template_name = 'polls/index.html'
+	context_object_name = 'latest_question_list'
 
-def detail(request, question_id):
-    return HttpResponse("You're looking at question %s." % question_id)
+	def get_queryset(self):
+		return Question.objects.order_by('-pub_date')[:5]
 
-def results(request, question_id):
-    response = "You're looking at the results of question %s."
-    return HttpResponse(response % question_id)
+class DetailView(generic.DetailView):
+	model = Question
+	template_name = 'polls/detail.html'
+
+class ResultsView(generic.DetailView):
+	model = Question
+	template_name = 'polls/results.html'
 
 def vote(request, question_id):
-    return HttpResponse("You're voting on question %s." % question_id)
+	p = get_object_or_404(Question, pk=question_id)
+	try:
+		selected_choice = p.choice_set.get(pk=request.POST['choice'])
+	except(KeyError, Choice.DoesNotExit):
+		return render(request, 'polls/detail.html', {
+			'question': p,
+			'error_message': "You didn't select a choice.",
+			})
+	else:
+		selected_choice.votes += 1
+		selected_choice.save()
+		return HttpResponseRedirect(reverse('polls:results', args=(p.id, )))
